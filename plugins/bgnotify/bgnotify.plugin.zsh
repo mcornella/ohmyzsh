7e608625ace8e50df36b55ff3539f518770a6932
Marc Cornellà <marc.cornella@live.com>
2019-04-07 20:27:46 +0200

    WIP

--- plugins/bgnotify/bgnotify.plugin.zsh
+++ plugins/bgnotify/bgnotify.plugin.zsh
@@ -1,49 +1,53 @@
-#!/usr/bin/env zsh
+# Requirements
 
-## setup ##
+[[ -o interactive ]] || return
 
-[[ -o interactive ]] || return #interactive only!
-zmodload zsh/datetime || { print "can't load zsh/datetime"; return } # faster than date()
-autoload -Uz add-zsh-hook || { print "can't add zsh hook!"; return }
+zmodload zsh/datetime || { print "bgnotify: can't load zsh/datetime"; return }
 
-(( ${+bgnotify_threshold} )) || bgnotify_threshold=5 #default 10 seconds
+# default 5 seconds
+: ${bgnotify_threshold:=5}
 
 
 ## definitions ##
 
-if ! (type bgnotify_formatted | grep -q 'function'); then ## allow custom function override
+if ! (( $+functions[bgnotify_formatted] )); then ## allow custom function override
   function bgnotify_formatted { ## args: (exit_status, command, elapsed_seconds)
     elapsed="$(( $3 % 60 ))s"
-    (( $3 >= 60 )) && elapsed="$((( $3 % 3600) / 60 ))m $elapsed"
+    (( $3 >= 60 )) && elapsed="$(( $3 / 60 % 60 ))m $elapsed"
     (( $3 >= 3600 )) && elapsed="$(( $3 / 3600 ))h $elapsed"
-    [ $1 -eq 0 ] && bgnotify "#win (took $elapsed)" "$2" || bgnotify "#fail (took $elapsed)" "$2"
+    [[ $1 -eq 0 ]] && bgnotify "#win (took $elapsed)" "$2" || bgnotify "#fail (took $elapsed)" "$2"
   }
 fi
 
 currentWindowId () {
-  if hash osascript 2>/dev/null; then #osx
-    osascript -e 'tell application (path to frontmost application as text) to id of front window' 2&> /dev/null || echo "0"
-  elif (hash notify-send 2>/dev/null || hash kdialog 2>/dev/null); then #ubuntu!
+  if (( $+commands[osascript] )); then # macOS
+    osascript -e 'tell application (path to frontmost application as text) to id of front window' 2> /dev/null || echo "0"
+  elif (( $+commands[notify-send] )) || (( $+commands[kdialog] )); then # Linux
     xprop -root 2> /dev/null | awk '/NET_ACTIVE_WINDOW/{print $5;exit} END{exit !$5}' || echo "0"
   else
-    echo $EPOCHSECONDS #fallback for windows
+    echo $EPOCHSECONDS # fallback for Windows
   fi
 }
 
 bgnotify () { ## args: (title, subtitle)
-  if hash terminal-notifier 2>/dev/null; then #osx
-    [[ "$TERM_PROGRAM" == 'iTerm.app' ]] && term_id='com.googlecode.iterm2';
-    [[ "$TERM_PROGRAM" == 'Apple_Terminal' ]] && term_id='com.apple.terminal';
-    ## now call terminal-notifier, (hopefully with $term_id!)
-    [ -z "$term_id" ] && terminal-notifier -message "$2" -title "$1" >/dev/null ||
-    terminal-notifier -message "$2" -title "$1" -activate "$term_id" -sender "$term_id" >/dev/null
-  elif hash growlnotify 2>/dev/null; then #osx growl
+  if (( $+commands[terminal-notifier] )); then # macOS
+    case "$TERM_PROGRAM" in
+      iTerm.app) term_id='com.googlecode.iterm2' ;;
+      Apple_Terminal) term_id='com.apple.terminal' ;;
+    esac
+
+    if [[ -z "$term_id" ]]; then
+      terminal-notifier -message "$2" -title "$1" > /dev/null
+    else
+      terminal-notifier -message "$2" -title "$1" -activate "$term_id" -sender "$term_id" > /dev/null
+    fi
+  elif (( $+commands[growlnotify] )); then # macOS growl
     growlnotify -m "$1" "$2"
-  elif hash notify-send 2>/dev/null; then #ubuntu gnome!
+  elif (( $+commands[notify-send] )); then # Gnome
     notify-send "$1" "$2"
-  elif hash kdialog 2>/dev/null; then #ubuntu kde!
+  elif (( $+commands[kdialog] )); then # KDE
     kdialog --title "$1" --passivepopup  "$2" 5
-  elif hash notifu 2>/dev/null; then #cygwyn support!
+  elif (( $+commands[notifu] )); then # Windows
     notifu /m "$2" /p "$1"
   fi
 }
@@ -60,9 +64,8 @@ bgnotify_begin() {
 bgnotify_end() {
   didexit=$?
   elapsed=$(( EPOCHSECONDS - bgnotify_timestamp ))
-  past_threshold=$(( elapsed >= bgnotify_threshold ))
-  if (( bgnotify_timestamp > 0 )) && (( past_threshold )); then
-    if [ $(currentWindowId) != "$bgnotify_windowid" ]; then
+  if (( bgnotify_timestamp > 0 )) && (( elapsed >= bgnotify_threshold )); then
+    if [[ $(currentWindowId) != "$bgnotify_windowid" ]]; then
       print -n "\a"
       bgnotify_formatted "$didexit" "$bgnotify_lastcmd" "$elapsed"
     fi
@@ -70,8 +73,8 @@ bgnotify_end() {
   bgnotify_timestamp=0 #reset it to 0!
 }
 
-## only enable if a local (non-ssh) connection
-if [ -z "$SSH_CLIENT" ] && [ -z "$SSH_TTY" ]; then
-  add-zsh-hook preexec bgnotify_begin
-  add-zsh-hook precmd bgnotify_end
+## only enable on a local (non-ssh) connection
+if [[ -z "$SSH_CLIENT" && -z "$SSH_TTY" ]]; then
+  preexec_functions+=(bgnotify_begin)
+  precmd_functions+=(bgnotify_end)
 fi
